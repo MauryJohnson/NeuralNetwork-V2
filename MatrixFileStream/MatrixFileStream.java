b081d40318b7d1fdfa3902db0c8e699f4c897a6d
Maury_Johnson <mauryljohnsonii@gmail.com>
2019-02-02 16:53:49 -0500

    Read File Test

--- MatrixFileStream/MatrixFileStream.java
+++ MatrixFileStream/MatrixFileStream.java
@@ -18,6 +18,12 @@ public class MatrixFileStream {
 	
 	FileInputStream F;
 	
+	String result = "";
+	
+	int Position = 0;
+	
+	FileChannel FC;
+	
 	Matrix M;
 	
 	//To rewind, use FileChannel fx = F.getChannel();
@@ -28,18 +34,45 @@ public class MatrixFileStream {
 		System.out.println("No file");
 	}
 	this.F=F;
+	FC = F.getChannel();
+
+	/*
+	try {
+		FC.close();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	*/
+	InputStreamReader I = new InputStreamReader(F);
+	this.result = new BufferedReader(I)
+			  .lines().collect(Collectors.joining("\n"));
+	
+	if(result==null) {
+		System.err.println("No File input");
+	}
+	try {
+		F.close();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	this.Delimiter=Delimiter;
 	}
 
-	public MatrixFileStream ReadFile(char Delimiter) {
+	public MatrixFileStream ReadFile() {
 		if(this.F==null) {
 			System.out.println("Unable to open file");
 			System.exit(-4);
 		}
-		FileInputStream F = this.F;
+		char Delimiter = this.Delimiter;
+		//FileInputStream F = this.F;
 		
 		int COLUMNS = this.GetColumns();
 		int ROWS = this.GetRows(Delimiter);
+		
+		System.out.printf("\n ROWS:%d",ROWS);
+		
 		int Columns = 0;
 		int Rows = 0;
 		
@@ -57,19 +90,21 @@ public class MatrixFileStream {
 		while(true) {
 			R = this.NextDouble(Delimiter);
 		
+			System.out.println("Return Status: ["+R[0]+","+R[1]+"]");
+			
 			if(R[0]==2||R[0]==1) {
 				
-				if(R[0]==1) {
+				//if(R[0]==1) {
 					
-					if(R[1]!=-88888888) {
+					if(R[1]!=-88888888 && R[1]!=-999999999) {
 					
 						System.out.printf("RESULT FINAL:%f",R[1]);
 					
 						Entries[Rows][Columns]=R[1];
 						
-					}
+				//}
 					
-				}
+					}
 				
 				System.out.println("DONE READING");
 				break;
@@ -88,15 +123,22 @@ public class MatrixFileStream {
 				
 				Columns+=1;
 			}
+			/*
+			else {
+				this.Position+=1;
+			}
+			*/
 			
 		}
 		
 		Rows+=1;
-		System.out.printf("\n SIZE:%f ROWS:%d COLUMNS:%d\n",Size,Rows,COLUMNS);
+		System.out.printf("\n SIZE:%d ROWS:%d COLUMNS:%d\n",Size,Rows,COLUMNS);
 		
 		if(COLUMNS==0) {
 			return null;
 		}
+		
+		this.NextLine();
 		
 		Matrix MNew = new Matrix(Matrix.NewDoubleMatrix(Entries),"Read M");
 		
@@ -105,6 +147,21 @@ public class MatrixFileStream {
 		return this;
 	}
 	
+	private void NextLine() {
+		// TODO Auto-generated method stub
+		int i=this.Position;
+		char c;
+		while(i<result.length()) {
+			c = result.charAt(i);
+			if(c=='\n') {
+				i+=1;
+				break;
+			}
+			i+=1;
+		}
+		this.Position = i;
+	}
+
 	private double[] NextDouble(char Delimiter) {
 		// TODO Auto-generated method stub
 		FileInputStream F = this.F;
@@ -116,12 +173,14 @@ public class MatrixFileStream {
 		int ASize = 1;
 		String app = "";
 		char c;
-		int i=0;
-		String result = new BufferedReader(new InputStreamReader(F))
-				  .lines().collect(Collectors.joining("\n"));
+		int i=this.Position;
+	
+		System.out.println("File:\n "+result+" Position:"+i);
 		
 		while(i<result.length()) {
 			c = result.charAt(i);
+			System.out.println("CHAR:"+c);
+			System.out.printf("\nApp:%s Size:%d\n ", app,ASize);
 			if(!Character.isDigit(c)) {
 				if(c=='.') {
 					if(Dot) {
@@ -133,13 +192,27 @@ public class MatrixFileStream {
 					Dot=true;
 				}
 				else if(c==Delimiter) {
-					R[1] = Double.parseDouble(app);
+					System.out.println(c + "1==" +Delimiter);
+					try {
+						R[1] = Double.parseDouble(app);
+						}
+						catch(Exception e){
+						R[1]=-88888888;
+					}
 					R[0]=1;
+					this.Position=i+1;
 					return R;
 				}
 				else {
+					System.out.println(c + "2==" +Delimiter);
+					try {
 					R[1] = Double.parseDouble(app);
+					}
+					catch(Exception e){
+					R[1]=-88888888;
+					}
 					R[0]=0;
+					this.Position=i+1;
 					return R;
 				}
 			}
@@ -151,15 +224,22 @@ public class MatrixFileStream {
 		}
 		
 		R[0]=2;
-		R[1]=-999999999;
-		app=null;
-		result=null;
+		try {
+			R[1] = Double.parseDouble(app);
+			}
+			catch(Exception e){
+			R[1]=-999999999;
+		}
+		//app=null;
+		//result=null;
+		this.Position=i;
 		return R;
 	}
 
 	public int GetColumns() {
+		/*
 		FileInputStream F = this.F;
-		FileChannel fc = F.getChannel();
+		FileChannel fc = this.FC;
 		long PrevPosition = 0;
 		try {
 			PrevPosition = fc.position();
@@ -168,15 +248,13 @@ public class MatrixFileStream {
 			e.printStackTrace();
 			System.exit(-4);
 		}
-	
+		 */
 		char c;
 		int C=0;
-		int i = 0;
+		int i = this.Position;
 		boolean Spaces=false;
 		boolean Dot=false;
 		
-		String result = new BufferedReader(new InputStreamReader(F))
-				  .lines().collect(Collectors.joining("\n"));
 		while(i<result.length()) {
 			c = result.charAt(i);
 			if(Character.isDigit(c)) {
@@ -207,6 +285,7 @@ public class MatrixFileStream {
 					
 					if(c=='\n') {
 						System.out.printf("\nMade it to final column:%d\n",C);
+						break;
 					}
 					
 				}
@@ -219,6 +298,7 @@ public class MatrixFileStream {
 			i+=1;
 		}
 		
+		/*
 		try {
 			fc.position(PrevPosition);
 		} catch (IOException e) {
@@ -226,14 +306,17 @@ public class MatrixFileStream {
 			e.printStackTrace();
 			System.exit(-4);
 		}
-		
-		result=null;
+		*/
+		//result=null;
+		//this.Position=i;
 		return C;
 	}
 	
 	public int GetRows(char Delimiter) {
-		FileInputStream F = this.F;
-		FileChannel fc = F.getChannel();
+		//FileInputStream F = this.F;
+		//FileChannel fc = this.FC;
+		
+		/*
 		long PrevPosition = 0;
 		try {
 			PrevPosition = fc.position();
@@ -242,33 +325,38 @@ public class MatrixFileStream {
 			e.printStackTrace();
 			System.exit(-4);
 		}
+		*/
 		
 		char c;
 		int R=0;
 		boolean DelimFound=false;
 		
-		int i=0;
-
-		String result = new BufferedReader(new InputStreamReader(F))
-				  .lines().collect(Collectors.joining("\n"));
+		boolean nums = false;
 		
-		while(i<result.length()) {
-		c = result.charAt(i);
-		if(c=='\n') {
-			if(!DelimFound) {
+		int i=Position;
+
+		while(i<this.result.length()) {
+		c = this.result.charAt(i);
+		if(c=='\n' || i==this.result.length()-1) {
+			if(nums) {
 				R+=1;
 			}
-			else {
-				DelimFound=false;
+			if(DelimFound) {
+				break;
 			}
+			nums=false;
 		}
 		else if(c==Delimiter) {
 			DelimFound=true;
-			R+=1;
+		}
+		else if(Character.isDigit(c)) {
+			nums=true;
 		}
 		i+=1;
 		}
 		
+		
+		/*
 		try {
 			fc.position(PrevPosition);
 		} catch (IOException e) {
@@ -276,21 +364,33 @@ public class MatrixFileStream {
 			e.printStackTrace();
 			System.exit(-4);
 		}
+		*/
+		/*
+		try {
+			fc.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
 		
-		result=null;
+		//result=null;
+		//this.Position=PrevPosition;
 		return R;
 	}
 	
 	public static void main(String[] args) {
 		MatrixFileStream MFS1 = null;
 		try {
-			MFS1 = new MatrixFileStream(new FileInputStream("./Model/Model1"),'_');
+			MFS1 = new MatrixFileStream(new FileInputStream("./TestMatrix"),'_');
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		MFS1.ReadFile('_');
+		MFS1.ReadFile();
+	System.out.println(MFS1.M);
+	MFS1.ReadFile();
 	System.out.println(MFS1.M);
 		
 	}
